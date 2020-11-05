0d31baa22966b4c903e90f8532abbe62f0f7e433
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-05 15:36:42 +0100

    added reset and index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Provider } from "react-redux"
+import store from "../reducers-actions/store"
+import "./index.css"
+import App from "../components/App"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Index() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
 }
