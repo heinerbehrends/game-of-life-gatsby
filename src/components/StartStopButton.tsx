2ee35fcdc6af8e9dea0f4898dabec302636bfe44
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-11 16:23:21 +0100

    tweaked styles

--- src/components/StartStopButton.tsx
+++ src/components/StartStopButton.tsx
@@ -2,15 +2,8 @@ import React from "react"
 import styled from "styled-components"
 import { useDispatch, useSelector } from "react-redux"
 import { DELAY } from "../reducers-actions/action-types"
+import { InputStyled } from "./Options"
 
-const StyledStartStopButton = styled.button`
-  background-color: white;
-  font-size: 2em;
-  width: 4em;
-  border: 1px solid lightblue;
-  margin: 0.5em;
-  padding: 1rem;
-`
 // default state is set in store.js
 function StartStopButton() {
   const delay = useSelector(state => state.delayReducer)
@@ -19,13 +12,14 @@ function StartStopButton() {
     dispatch({ type: DELAY, delay })
   }
   return (
-    <StyledStartStopButton
+    <InputStyled
+      as="button"
       onClick={() => {
         delay ? setDelay(null) : setDelay(200)
       }}
     >
       {delay ? "Stop" : "Start"}
-    </StyledStartStopButton>
+    </InputStyled>
   )
 }
 
