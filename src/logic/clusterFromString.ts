65f3af1194e0dbbaf801caf1e83a1881c3c8605c
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-11 16:22:26 +0100

    function that parses strings from conwaylife.com

--- src/logic/clusterFromString.ts
+++ src/logic/clusterFromString.ts
@@ -1,35 +1,33 @@
-function getXValues(clusterString: string) {
-  return Array.from(clusterString).reduce((acc: number[], curr: string, idx: number) => {
-    if (curr === "O") {
+import { matrixT } from "./liveNeighbours";
+
+export const clusterFromString = (clusterString: string, xy: number[]) => {
+  const cluster: number[][] = clusterString.split(/\n/).map(getXValues).map(getYValues).flat()
+  return addXY(xy, cluster)
+}
+
+function getXValues(row: string): number[] {
+  return Array.from(row).reduce((acc: number[], letter: string, idx: number) => {
+    if (letter === "O") {
       acc.push(idx)
     }
     return acc
   }, [])
 }
-function getYValues(arr, yValue) {
-  return arr.map(xValue => [xValue, yValue])
+function getYValues(rows, yValue): number[][] {
+  return rows.map(xValue => [xValue, yValue])
 }
-function addXY([x, y]: number[], arrayOfXYs) {
+function addXY([x, y]: number[], arrayOfXYs: number[][]) {
   return arrayOfXYs.map(([x2, y2]) => [x + x2, y + y2])
 }
-
-export const rPentominoString = `.............OO
-............O.O
-...........O...
-..........O....
-.........O.....
-........O......
-.......O.......
-......O........
-.....O.........
-....O..........
-...O...........
-..O............
-OO.............`
-console.log(rPentominoString.split(/\n/))
-
-export const clusterFromString = (clusterString: string) => (xy: number[]) => {
-  const cluster = clusterString.split(/\n/).map(getXValues).map(getYValues).flat()
-  return addXY(xy, cluster)
+function foldTorus([x,y]: number[], matrix: matrixT ) {
+  const rowLength = matrix[0].length
+  const columnLength = matrix.length
+  if (x > rowLength) {
+    x = x - rowLength
+  } if (y > columnLength) {
+    y = y - columnLength
+  }
+  return [x,y]
 }
+
 
