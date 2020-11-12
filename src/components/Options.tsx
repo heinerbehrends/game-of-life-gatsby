5b87ab38fcdf9fb9bf3bcae3f0d9c9577e317ea3
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-12 16:09:34 +0100

    Added slider and pattern menu state

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,33 +1,66 @@
-import React from "react"
+import React, { useState } from "react"
 import { useDispatch } from "react-redux"
 import styled from "styled-components"
+import PatternButtons from "./PatternButtons"
+import Slider from "./Slider"
+import StartStopButton from "./StartStopButton"
 
-// const Button = styled.button`
-//   position: absolute;
-//   padding: 0.5rem;
-//   font-size: 16px;
-// `
-const Input = styled.input`
-  padding: 0.5rem;
+export const InputStyled = styled.input`
+  margin-left: 8px;
+  padding: 0.2rem 0.5rem 0.2rem 0.5rem;
+  border: 1px solid lightblue;
+  border-radius: 8px;
+  font-size: 16px;
+  background-color: white;
 `
-const Container = styled.menu`
+const OptionsContainer = styled.div`
   background-color: #eee;
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
+  align-items: center;
+  height: 40px;
+`
+type dropDownButtonProps = {
+  onClick: Function
+}
+
+const DropDownButton = styled.button<dropDownButtonProps>`
+  section {
+    flex-direction: column;
+  }
 `
 
-function Options(props) {
+function Options() {
   const dispatch = useDispatch()
+  const [sliderValue, setSliderValue] = useState("4")
+  const [isOpen, setOpen] = useState(true)
   return (
-    <Container>
-      <Input
-        placeholder={"frames per second"}
-        onChange={event => {
-          dispatch({ type: "DELAY", delay: 600 / event.target.value })
-        }}
-      />
-    </Container>
+    <>
+      <OptionsContainer>
+        <label htmlFor="speed">Speed</label>
+        <Slider
+          onInput={event => {
+            dispatch({
+              type: "DELAY",
+              delay:
+                600 / event.target.value === Infinity
+                  ? null
+                  : 600 / event.target.value,
+            })
+          }}
+          onChange={event => {
+            setSliderValue(event.target.value)
+          }}
+          value={sliderValue}
+        />
+        <StartStopButton />
+        <DropDownButton onClick={() => setOpen(!isOpen)}>
+          Patterns
+        </DropDownButton>
+      </OptionsContainer>
+      <PatternButtons isOpen={isOpen} />
+    </>
   )
 }
 
