a5cf45bbf9725abaa3d022b858f5c3322ca8b668
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-12 16:03:07 +0100

    added patterns logic

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,25 +1,34 @@
 import React, { useCallback, memo } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import styled from "styled-components"
-import { longEvolveCluster } from "../reducers-actions/action-creators"
+import { patternSwitch } from "../patterns/patterns"
 
-const StyledSquare = styled.div`
+const StyledSquare = styled.div<cellProps>`
   width: 8px;
   height: 8px;
   display: inline-block;
   border: 1px solid darkgray;
   background-color: ${props => (props.isAlive ? "yellow" : "black")};
+  box-sizing: border-box;
 `
 
-function Cell({ isAlive, x, y }) {
-  const dispatch = useDispatch()
-  const setCluster = useCallback(() => dispatch(longEvolveCluster([x, y])), [
-    dispatch,
-    x,
-    y,
-  ])
+type cellProps = {
+  isAlive: boolean
+  x: number
+  y: number
+}
+
+const patternSelector = state => state.patternReducer
+
+function Cell({ isAlive, x, y }: cellProps): React.ReactElement {
+  const dispatch: Function = useDispatch()
+  const pattern: string = useSelector(patternSelector)
+  const setPattern = useCallback(
+    () => dispatch(patternSwitch[pattern]([x, y])),
+    [x, y, pattern]
+  )
   const handleClick = () => {
-    setCluster()
+    setPattern()
   }
   return <StyledSquare isAlive={isAlive} x={x} y={y} onClick={handleClick} />
 }
