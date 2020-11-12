401ca1cef29542d31e2674baf22ad3e173eeaab8
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-12 16:03:50 +0100

    Added spinner, removed useMemo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useMemo } from "react"
-import Matrix from "./Matrix"
+import React, { useCallback, useEffect, useMemo } from "react"
+import { createGlobalStyle } from "styled-components"
+import Board from "./Board"
 import Options from "./Options"
 import { useDispatch, useSelector } from "react-redux"
 import useInterval from "../logic/useInterval"
@@ -7,15 +8,18 @@ import nextGen from "../logic/nextGen"
 import { matrixAction } from "../reducers-actions/action-creators"
 import { drawInitialMatrix } from "../reducers-actions/matrix-reducer"
 import "./App.css"
+import arraysAreEqual from "../logic/arraysAreEqual"
+import ClassicSpinner from "./Spinnner"
+
+const getMatrix = state => state.matrixReducer
+const getDelay = state => state.delayReducer
 
 function App() {
   // setup state and dispatch
   const dispatch = useDispatch()
-  const matrix = useSelector(state => state.matrixReducer)
-  const setMatrix = matrix => dispatch(matrixAction(matrix))
-  const delay = useSelector(state => state.delayReducer)
+  const matrix = useSelector(getMatrix)
   // setup matrix width to fill the viewport
-  useEffect(() => {
+  useEffect((): void => {
     const vw = Math.max(
       document.documentElement.clientWidth || 0,
       window.innerWidth || 0
@@ -26,20 +30,31 @@ function App() {
     )
 
     const initialMatrix = drawInitialMatrix(
-      Math.floor(vh / 10.8),
-      Math.floor(vw / 10)
+      Math.floor((vh - 40) / 8),
+      Math.floor((vw - 16) / 8)
     )
     dispatch(matrixAction(initialMatrix))
-  }, [dispatch])
+  }, [])
+  const setMatrix = useCallback(matrix => dispatch(matrixAction(matrix)), [
+    matrix,
+  ])
+  const delay = useSelector(getDelay)
   // setup simulation
-  const next = useMemo(() => nextGen(matrix), [matrix])
+
   useInterval(() => {
-    setMatrix(next)
+    // console.log(matrix)
+    // console.log(nextGen(matrix))
+    const next = nextGen(matrix)
+    console.log()
+    if (!arraysAreEqual(matrix, next)) {
+      setMatrix(nextGen(matrix))
+    }
   }, delay)
+
   return (
     <>
       <Options />
-      {matrix ? <Matrix matrix={matrix} /> : null}
+      {matrix ? <Board matrix={matrix} /> : <ClassicSpinner />}
     </>
   )
 }
