603343e6868157f96516946ddae38bdb35f9cf0b
heinerbehrends <heinerbehrends@hotmail.com>
2020-11-12 16:06:46 +0100

    removed patterns

--- src/reducers-actions/action-creators.ts
+++ src/reducers-actions/action-creators.ts
@@ -1,25 +1,4 @@
-import { MATRIX, CLUSTER, PATTERN } from "./action-types"
-import { clusterFromString } from "../logic/clusterFromString"
-import { acornString, dieHardString, rPentominoString } from "../patterns";
-
-
-type clusterAction = {
-  type: string
-  cluster: number[][]
-}
-// Methuselahs
-export const rPentominoCluster = (xy: number[]): clusterAction => ({
-  type: CLUSTER,
-  cluster: clusterFromString(rPentominoString, xy),
-})
-export const dieHardCluster = (xy: number[]): clusterAction => ({
-  type: CLUSTER,
-  cluster: clusterFromString(dieHardString, xy),
-})
-export const acornCluster = (xy: number[]): clusterAction => ({
-  type: CLUSTER,
-  cluster: clusterFromString(acornString, xy),
-})
+import { MATRIX, PATTERN } from "./action-types"
 
 
 export const matrixAction = matrix => ({
