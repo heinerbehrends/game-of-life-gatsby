48ca64790c8a23531a7aa42b3fbe3a486265b32e
heinerbehrends <heinerbehrends@hotmail.com>
2021-02-16 09:14:30 +0100

    added cellSize prop, variable cellSize

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -2,10 +2,11 @@ import React, { useCallback, memo } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import styled from "styled-components"
 import { patternSwitch } from "../patterns/patterns"
+import { getCellSize } from "./App"
 
 const StyledSquare = styled.div<cellProps>`
-  width: 8px;
-  height: 8px;
+  width: ${({ cellSize }) => cellSize}px;
+  height: ${({ cellSize }) => cellSize}px;
   display: inline-block;
   border: 1px solid darkgray;
   background-color: ${props => (props.isAlive ? "yellow" : "black")};
@@ -16,6 +17,7 @@ type cellProps = {
   isAlive: boolean
   x: number
   y: number
+  cellSize: number
 }
 
 const patternSelector = state => state.patternReducer
@@ -23,6 +25,7 @@ const patternSelector = state => state.patternReducer
 function Cell({ isAlive, x, y }: cellProps): React.ReactElement {
   const dispatch: Function = useDispatch()
   const pattern: string = useSelector(patternSelector)
+  const cellSize: number = useSelector(getCellSize)
   const setPattern = useCallback(
     () => dispatch(patternSwitch[pattern]([x, y])),
     [x, y, pattern]
@@ -30,7 +33,15 @@ function Cell({ isAlive, x, y }: cellProps): React.ReactElement {
   const handleClick = () => {
     setPattern()
   }
-  return <StyledSquare isAlive={isAlive} x={x} y={y} onClick={handleClick} />
+  return (
+    <StyledSquare
+      isAlive={isAlive}
+      x={x}
+      y={y}
+      onClick={handleClick}
+      cellSize={cellSize}
+    />
+  )
 }
 
 export default memo(Cell)
