00e09d1bbe2a1af1dbddd99f20e9f44aa2ef4c39
heinerbehrends <heinerbehrends@hotmail.com>
2021-02-16 09:13:47 +0100

    added styled container div, style prop

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,13 @@
 import React from "react"
 import Row from "./Row"
 
-function Board({ matrix }) {
-  return matrix.map((row, index) => (
-    <Row rowList={row} key={index} rowKey={index} />
-  ))
+function Board({ matrix, style }) {
+  return (
+    <div style={{ ...style }}>
+      {matrix.map((row, index) => (
+        <Row rowList={row} key={index} rowKey={index} />
+      ))}
+    </div>
+  )
 }
 export default React.memo(Board)
