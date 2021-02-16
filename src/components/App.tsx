a97d2f490611d9058ec051e1d4e69cc23afdd2a0
heinerbehrends <heinerbehrends@hotmail.com>
2021-02-16 09:12:05 +0100

    extracted useViewport, renamed matrixAction

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useEffect, useMemo } from "react"
+import React, { useCallback, useEffect } from "react"
 import Board from "./Board"
 import Options from "./Options"
 import { useDispatch, useSelector } from "react-redux"
 import useInterval from "../logic/useInterval"
 import nextGen from "../logic/nextGen"
-import { matrixAction } from "../reducers-actions/action-creators"
+import { createMatrixAction } from "../reducers-actions/action-creators"
 import { drawInitialMatrix } from "../reducers-actions/matrix-reducer"
 import "./App.css"
 import arraysAreEqual from "../logic/arraysAreEqual"
@@ -12,37 +12,46 @@ import ClassicSpinner from "./Spinnner"
 
 const getMatrix = state => state.matrixReducer
 const getDelay = state => state.delayReducer
+export const getCellSize = state => state.cellSizeReducer
+
+function useViewport() {
+  const vw = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  )
+  const vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  )
+  return { vw, vh }
+}
 
 function App() {
   // setup state and dispatch
   const dispatch = useDispatch()
-  const matrix = useSelector(getMatrix)
+  // const matrix = useSelector(getMatrix)
   // setup matrix width to fill the viewport
-  useEffect((): void => {
-    const vw = Math.max(
-      document.documentElement.clientWidth || 0,
-      window.innerWidth || 0
-    )
-    const vh = Math.max(
-      document.documentElement.clientHeight || 0,
-      window.innerHeight || 0
-    )
+  const cellSize = useSelector(getCellSize)
+  const topBarHeight = 40
 
+  useEffect(() => {
+    const { vw, vh } = useViewport()
     const initialMatrix = drawInitialMatrix(
-      Math.floor(vh / 8),
-      Math.floor(vw / 8)
+      Math.floor((vh - topBarHeight) / cellSize),
+      Math.floor(vw / cellSize)
     )
-    dispatch(matrixAction(initialMatrix))
-  }, [])
-  const setMatrix = useCallback(matrix => dispatch(matrixAction(matrix)), [
-    matrix,
-  ])
-  const delay = useSelector(getDelay)
+    dispatch(createMatrixAction(initialMatrix))
+  }, []) // useViewportMatrix()
   // setup simulation
+  const delay = useSelector(getDelay)
+  const matrix = useSelector(getMatrix)
+  const setMatrix = useCallback(
+    matrix => dispatch(createMatrixAction(matrix)),
+    [matrix]
+  )
 
   useInterval(() => {
     const next = nextGen(matrix)
-    console.log()
     if (!arraysAreEqual(matrix, next)) {
       setMatrix(nextGen(matrix))
     }
@@ -51,7 +60,11 @@ function App() {
   return (
     <>
       <Options />
-      {matrix ? <Board matrix={matrix} /> : <ClassicSpinner />}
+      {matrix ? (
+        <Board style={{ marginTop: topBarHeight }} matrix={matrix} />
+      ) : (
+        <ClassicSpinner />
+      )}
     </>
   )
 }
